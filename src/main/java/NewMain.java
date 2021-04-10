ad91ca337ec8e9dee7d53c8864778e3fc9976c36
Rhassan R.Santos <rhassanrodrigues6@gmail.com>
2021-04-10 00:11:56 -0300

    testando

---
+++ src/main/java/NewMain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NewMain {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner   upoald_date = new Scanner(System.in); // 
+        
+            String nome = upoald_date.next();
+            System.err.println(nome);
+        
+    }
+    
+}
